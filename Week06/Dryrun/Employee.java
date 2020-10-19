1554fabd1bafdb981fcf951bfc50821e12ceffa3
LuDotDoc <17019043@uhi.ac.uk>
2020-10-19 13:24:44 +0100

    Testing Dry run code

---
+++ Week06/Dryrun/Employee.java
@@ -0,0 +1,46 @@
+package Week06.Dryrun;
+
+/*************************************************************************
+ *                           Luke Docwra 17019043                         *
+ *                             2020 HND Tutorial                          *
+ *                               Week06.Dryrun                          *
+ *************************************************************************/
+
+public class Employee {
+    private String name;
+    private String address;
+    private int number;
+    public Employee(String name, String address, int number)
+    {
+        System.out.println("Constructing an Employee");
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+    public void mailCheck()
+    {
+        System.out.println("Mailing a check to " + this.name + " " + this.address);
+    }
+    public String toString()
+    {
+        return (name + " " + address + " " + number);
+    }
+    public String getName()
+    {
+        return (name);
+    }
+    public String getAddress()
+    {
+        return (address);
+    }
+    public void setAddress(String newAddress)
+    {
+        address = newAddress;
+    }
+    public int getNumber()
+    {
+        return (number);
+    }
+}
+
+                                                                           
