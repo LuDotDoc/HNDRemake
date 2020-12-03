f7abea9514c38bfa1f1926e31c1f8871c8db8d63
LuDotDoc <17019043@uhi.ac.uk>
2020-12-03 04:00:50 +0000

    Forgot to add experimental directories. Whoops

---
+++ Week08/TestingCode/DumbMaths/Main.java
@@ -0,0 +1,18 @@
+package Week08.TestingCode.DumbMaths;
+
+/*************************************************************************
+ *                           Luke Docwra 17019043                         *
+ *                             2020 HND Tutorial                          *
+ *                               Week08.TestingCode.DumbMaths                          *
+ *************************************************************************/
+
+public class Main {
+    public static void main(String[] args) {
+        int total = 6/2*(2+1);
+        System.out.println(total);
+    }
+
+
+}
+
+                                                                           
